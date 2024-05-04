ae70f367f199fe618905d8703cfd89ca2e42d0b1
Inyong <tlsdlsdyd98@gmail.com>
2024-05-05 02:37:02 +0900

    OpenGL, glm 추가

--- Engine/Engine/main.cpp
+++ Engine/Engine/main.cpp
@@ -1,7 +1,48 @@
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+
 }
