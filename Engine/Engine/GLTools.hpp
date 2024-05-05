ab562bdfa5a66ea5f62c7a9c08d70b02b9125898
Inyong <tlsdlsdyd98@gmail.com>
2024-05-05 18:20:16 +0900

    OpenGL 에러 출력하는 함수 추가

--- Engine/Engine/GLTools.hpp
+++ Engine/Engine/GLTools.hpp
@@ -5,5 +5,15 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#include <iostream>
+
+inline void glErr(const std::string& message) {
+	GLint err = glGetError();
+	if (err != GL_NO_ERROR) {
+		printf("%08X ", err);
+		std::cerr << "GL Error: " << message << std::endl;
+	}
+}
+
 
 #endif /* GLTOOLS_HPP */
