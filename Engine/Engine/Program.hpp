745c8f5437d4e6571ce255609ca2704492f74612
Inyong <tlsdlsdyd98@gmail.com>
2024-05-09 23:38:54 +0900

    Shader로 uniform 변수 넘겨주는 기능 추가

--- Engine/Engine/Program.hpp
+++ Engine/Engine/Program.hpp
@@ -35,6 +35,50 @@ public:
 		loadShaders(vertexPath, fragmentPath, geometryPath, tessControlPath, tessEvaluatePath);
 	}
 	virtual ~Program() { clear(); }
+
+	void setUniform(const std::string& name, float v) const {
+		glUniform1f(glGetUniformLocation(programID, name.c_str()), v);
+	}
+	void setUniform(const std::string& name, int v) const {
+		glUniform1i(glGetUniformLocation(programID, name.c_str()), v);
+	}
+	void setUniform(const std::string& name, const glm::vec2& v) const {
+		glUniform2f(glGetUniformLocation(programID, name.c_str()), v.x, v.y);
+	}
+	void setUniform(const std::string& name, const glm::vec3& v) const {
+		glUniform3f(glGetUniformLocation(programID, name.c_str()), v.x, v.y, v.z);
+	}
+	void setUniform(const std::string& name, const glm::vec4& v) const {
+		glUniform4f(glGetUniformLocation(programID, name.c_str()), v.x, v.y, v.z, v.w);
+	}
+	void setUniform(const std::string& name, const glm::mat3& v) const {
+		glUniformMatrix3fv(glGetUniformLocation(programID, name.c_str()), 1, GL_FALSE, value_ptr(v));
+	}
+	void setUniform(const std::string& name, const glm::mat4& v) const {
+		glUniformMatrix4fv(glGetUniformLocation(programID, name.c_str()), 1, GL_FALSE, value_ptr(v));
+	}
+	void setUniform(const std::string& name, const float* v, uint32_t count) const {
+		glUniform1fv(glGetUniformLocation(programID, name.c_str()), count, v);
+	}
+	void setUniform(const std::string& name, const int* v, uint32_t count) const {
+		glUniform1iv(glGetUniformLocation(programID, name.c_str()), count, v);
+	}
+	void setUniform(const std::string& name, const glm::vec2* v, uint32_t count) const {
+		glUniform2fv(glGetUniformLocation(programID, name.c_str()), count, (float*)v);
+	}
+	void setUniform(const std::string& name, const glm::vec3* v, uint32_t count) const {
+		glUniform3fv(glGetUniformLocation(programID, name.c_str()), count, (float*)v);
+	}
+	void setUniform(const std::string& name, const glm::vec4* v, uint32_t count) const {
+		glUniform4fv(glGetUniformLocation(programID, name.c_str()), count, (float*)v);
+	}
+	void setUniform(const std::string& name, const glm::mat3* v, uint32_t count) const {
+		glUniformMatrix3fv(glGetUniformLocation(programID, name.c_str()), count, GL_FALSE, (float*)v);
+	}
+	void setUniform(const std::string& name, const glm::mat4* v, uint32_t count) const {
+		glUniformMatrix4fv(glGetUniformLocation(programID, name.c_str()), count, GL_FALSE, (float*)v);
+	}
+
 };
 
 
