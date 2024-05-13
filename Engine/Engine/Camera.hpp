38a8466476a2707a41d79142898bc861a7d3df73
Inyong <tlsdlsdyd98@gmail.com>
2024-05-13 23:21:21 +0900

    fov를 radian 표기법으로 사용

--- Engine/Engine/Camera.hpp
+++ Engine/Engine/Camera.hpp
@@ -12,7 +12,7 @@ private:
 
 	glm::vec3 upVector = glm::vec3(0.f,1.f,0.f);
 
-	float fov = 45.f;
+	float fov = 45.f * glm::pi<float>() / 180.f;
 	float zNear = 0.1f;
 	float zFar = 1000.f;
 	glm::vec2 viewport = glm::vec2(0.f);
