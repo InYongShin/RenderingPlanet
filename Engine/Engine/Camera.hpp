757ddce4c1b91e9a00e71e60768791cbb6788bf8
Inyong <tlsdlsdyd98@gmail.com>
2024-05-14 23:41:42 +0900

    Camera getter setter 추가, 회전 위한 변수 추가

--- Engine/Engine/Camera.hpp
+++ Engine/Engine/Camera.hpp
@@ -13,6 +13,7 @@ private:
 	glm::vec3 upVector = glm::vec3(0.f,1.f,0.f);
 
 	float fov = 45.f * glm::pi<float>() / 180.f;
+	float yaw = 0, roll = 0, pitch = 0;
 	float zNear = 0.1f;
 	float zFar = 1000.f;
 	glm::vec2 viewport = glm::vec2(0.f);
@@ -20,6 +21,16 @@ private:
 public:
 
 	inline void setPosition(const glm::vec3& position) { this->position = position; }
+	inline glm::vec3 getPosition() const { return this->position; }
+	inline void setCenter(const glm::vec3& center) { this->center = center; }
+	inline float getFov() const { return this->fov; }
+	inline void setFov(float fov) { this->fov = fov; }
+	inline float getYaw() const { return this->yaw; }
+	inline void setYaw(float yaw) { this->yaw = yaw; }
+	inline float getRoll() const { return this->roll; }
+	inline void setRoll(float roll) { this->roll = roll; }
+	inline float getPitch() const { return this->pitch; }
+	inline void setPitch(float pitch) { this->pitch = pitch; }
 	inline void setViewport(const glm::vec2& viewport) { this->viewport = viewport; }
 
 	glm::mat4 projMat() const {
