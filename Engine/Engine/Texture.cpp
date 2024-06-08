1972a0a5d551ee5a60550055ecc56f60d052a7df
Inyong <tlsdlsdyd98@gmail.com>
2024-06-08 20:32:05 +0900

    Texture 로드 및 GL 관련 함수 구현

--- Engine/Engine/Texture.cpp
+++ Engine/Engine/Texture.cpp
@@ -1,3 +1,169 @@
 
 #include "Texture.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+std::tuple<GLenum,GLenum,GLenum> Texture::getTextureType(GLenum type, int numChannels, bool sRGB /*= false*/)
+{
+	switch(type)
+	{
+		case GL_UNSIGNED_SHORT:
+		{
+			switch(numChannels)
+			{
+				case 1: return {GL_R16UI, GL_RED, type};
+				case 2:	return {GL_RG16UI, GL_RG, type};
+				case 4:	return {GL_RGBA16UI, GL_RGBA, type};
+				case 3:
+				default: return {GL_RGB16UI, GL_RGB, type};
+			}
+			break;
+		}
+		case GL_UNSIGNED_INT:
+		{
+			switch(numChannels)
+			{
+				case 1:	return {GL_R32UI, GL_RED, type};
+				case 2:	return {GL_RG32UI, GL_RG, type};
+				case 4:	return {GL_RGBA32UI, GL_RGBA, type};
+				case 3:
+				default: return {GL_RGB32UI, GL_RGB, type};
+			}
+			break;
+		}
+		case GL_FLOAT:
+		{
+			switch(numChannels)
+			{
+				case 1:	return {GL_R32F, GL_RED, type};
+				case 2:	return {GL_RG32F, GL_RG, type};
+				case 4:	return {GL_RGBA32F, GL_RGBA, type};
+				case 3:
+				default: return {GL_RGB32F, GL_RGB, type};
+			}
+			break;
+		}
+		case GL_UNSIGNED_BYTE:
+		default:
+		{
+			switch(numChannels)
+			{
+				case 1:	return {GL_R8, GL_RED, type};
+				case 2:	return {GL_RG8, GL_RG, type};
+				case 4:	return {GLenum(sRGB?GL_SRGB8_ALPHA8:GL_RGBA8), GL_RGBA, type};
+				case 3:
+				default: return {GLenum(sRGB?GL_SRGB8:GL_RGB8), GL_RGB, type};
+			}
+			break;
+		}
+	}
+}
+
+void Texture::setTexParam(GLuint minFilter /*= GL_LINEAR*/, GLuint wrap_s /*= GL_REPEAT*/, GLuint wrap_t /*= GL_REPEAT*/)
+{
+	float maxAniso;
+	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAniso);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, maxAniso);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_s);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_t);
+}
+
+void Texture::load(const std::string& fileName, const bool isSrgb /*= false*/, const bool isNeedMaintainData /*= false*/)
+{
+	std::cout << "Loading texture: " << fileName << "\n";
+	
+	int width=0, height=0, numChannels=0;
+	_isSrgb = isSrgb;
+	_isNeedMaintainData = isNeedMaintainData;
+
+	stbi_set_flip_vertically_on_load(true);
+
+	_data = stbi_load(fileName.c_str(), &width, &height, &numChannels, 0);
+	if (_data == nullptr)
+	{
+		std::cerr << "Failed to load texture: " << fileName << std::endl;
+		return;
+	}
+
+	_width = width;
+	_height = height;
+	_numChannels = numChannels;
+
+	createGL();
+}
+
+void Texture::createGL()
+{
+	if (_texID > 0)
+	{
+		clear();
+	}
+
+	// TODO: type
+	auto [internalFormat, format, type] = getTextureType(_type, _numChannels, _isSrgb);
+	GLint oldTexID = Texture::getBinding();
+	
+	glGenTextures(1, &_texID);
+	glBindTexture(GL_TEXTURE_2D, _texID);
+	setTexParam(_minFilter, _wrap_s, _wrap_t);
+	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, _width, _height, 0, format, type, _data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	Texture::restoreBinding(oldTexID);
+
+	if (_data != nullptr && _isNeedMaintainData == false)
+	{
+		delete _data;
+		_data = nullptr;
+	}
+}
+
+void Texture::bind(int slot)
+{
+	if (_texID < 1)
+	{
+		std::cerr << "Texture is not loaded" << std::endl;
+		return;
+	}
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(GL_TEXTURE_2D, _texID);
+}
+
+void Texture::bind(int slot, const Program& program, const std::string& name)
+{
+	if (_texID < 1)
+	{
+		std::cerr << "Texture is not loaded" << std::endl;
+		return;
+	}
+
+	if (program.isUsable() == false)
+	{
+		std::cerr << "Program is not usable" << std::endl;
+		return;
+	}
+
+	bind(slot);
+	program.setUniform(name, slot);
+}
+
+void Texture::clear()
+{
+	if (_texID > 0)
+	{
+		glDeleteTextures(1, &_texID);
+		_texID = 0;
+	}
+
+	_width = 0;
+	_height = 0;
+	_numChannels = 0;
+
+	if (_data != nullptr)
+	{
+		delete _data;
+		_data = nullptr;
+	}
+}
--- Engine/Engine/Texture.hpp
+++ Engine/Engine/Texture.hpp
@@ -1,15 +1,70 @@
 #ifndef TEXTURE_HPP
 #define TEXTURE_HPP
 
+#include "GLTools.hpp"
+#include "Program.hpp"
+
+#include <iostream>
+#include <string>
 
 class Texture
 {
 private:
+	int _width = 0;
+	int _height = 0;
+	int _numChannels = 0;
+	unsigned char* _data = nullptr;
 
+	GLuint _texID = 0;
+	GLint _internalFormat = GL_RGB32UI;
+	GLenum _format = GL_RGB;
+	GLenum _type = GL_UNSIGNED_BYTE;
 
+	GLuint _minFilter = GL_LINEAR;
+	GLuint _wrap_s = GL_REPEAT;
+	GLuint _wrap_t = GL_REPEAT;
+
+	bool _isSrgb = false;
+
+	bool _isNeedMaintainData = false;
+
+	std::tuple<GLenum,GLenum,GLenum> getTextureType(GLenum type, int numChannels, bool sRGB = false);
+	void setTexParam(GLuint minFilter = GL_LINEAR, GLuint wrap_s = GL_REPEAT, GLuint wrap_t = GL_REPEAT);
+	void createGL();
+
+	static GLint getBinding()
+	{
+		GLint oldTexID = 0;
+		glGetIntegerv(GL_TEXTURE_BINDING_2D, &oldTexID);
+		return oldTexID;
+	}
+	static void restoreBinding(GLint oldTexID)
+	{
+		glBindTexture(GL_TEXTURE_2D, oldTexID);
+	}
+	
 public:
+	inline int getWidth() const { return _width; }
+	inline int getHeight() const { return _height; }
+	inline int getNumChannels() const { return _numChannels; }
 
+	inline int getTexID() const { return _texID; }
 
+	void load(const std::string& filenName, const bool isSrgb = false, const bool isNeedMaintainData = false);
+
+	void bind(int slot);
+	void bind(int slot, const Program& program, const std::string& name);
+
+	bool isUsable() const { return _texID > 0; }
+
+	void clear();
+
+	Texture() {}
+	Texture(const std::string& fileName) 
+	{
+		load(fileName);
+	}
+	virtual ~Texture() { clear(); }
 };
 
 
