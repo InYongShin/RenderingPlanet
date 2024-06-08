7bc64243277418ba024797f460461982a848fb8b
Inyong <tlsdlsdyd98@gmail.com>
2024-06-09 01:12:51 +0900

    태양계 구성

--- Engine/Engine/Application.cpp
+++ Engine/Engine/Application.cpp
@@ -54,28 +54,93 @@ void Application::run()
 
 
 	Program sunProgram("render.vert", "texture.frag");
-	Texture lightTexture("../Textures/Light.png");
-	lightTexture.bind(1, sunProgram, "tex");
-	std::shared_ptr<EmissivePlanet> sun = std::make_shared<EmissivePlanet>("Sun", glm::vec3(.8f, .8f, 0.f), .5f, 1.f, sunProgram);
-	spaceScene->addPlanet(sun);	
+	Texture sunTexture("../Textures/Sun.jpg");
+	sunTexture.bind(1, sunProgram, "tex");
+	std::shared_ptr<EmissivePlanet> sun = std::make_shared<EmissivePlanet>("Sun", glm::vec3(-12.f, 0.f, 0.f), 10.9f, 1.f, sunProgram);
+	spaceScene->addPlanet(sun);
 
+	Program mercuryProgram("render.vert", "render.frag");
+	Texture mercuryTexture("../Textures/Mercury.jpg");
+	mercuryTexture.bind(2, mercuryProgram, "tex");
+	std::shared_ptr<Planet> mercury = std::make_shared<Planet>("Mercury", glm::vec3(-0.61f, 0.f, 0.f), 0.04f, mercuryProgram);
+	mercuryProgram.setUniform("lightPosition", sun->getPosition());
+	mercuryProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(mercury);
 
-	Program planetProgram("render.vert", "render.frag");
-	std::shared_ptr<Planet> planet = std::make_shared<Planet>("Planet", glm::vec3(0.f, 0.f, -1.f), .3f, planetProgram);
-	planetProgram.setUniform("lightPosition", sun->getPosition());
-	planetProgram.setUniform("lightPower", sun->getEmissivePower());
+	Program venusProgram("render.vert", "render.frag");
+	Texture venusTexture("../Textures/Venus.jpg");
+	venusTexture.bind(3, venusProgram, "tex");
+	std::shared_ptr<Planet> venus = std::make_shared<Planet>("Venus", glm::vec3(-0.28f, 0.f, 0.f), .09f, venusProgram);
+	venusProgram.setUniform("lightPosition", sun->getPosition());
+	venusProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(venus);
 
+	Program earthProgram("render.vert", "render.frag");
+	Texture earthTexture("../Textures/Earth.jpg");
+	earthTexture.bind(4, earthProgram, "tex");
+	std::shared_ptr<Planet> earth = std::make_shared<Planet>("Earth", glm::vec3(0.f, 0.f, 0.f), 0.1f, earthProgram);
+	earthProgram.setUniform("lightPosition", sun->getPosition());
+	earthProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(earth);
 
-	Program billboardProgram("render.vert", "texture.frag");
-	Texture billboardTexture("../Textures/Billboard.png");
-	billboardTexture.bind(0, billboardProgram, "tex");
-	std::shared_ptr<QuadModel> quad = std::make_shared<QuadModel>();
-	quad->createQuad({-1.f, 1.f, 0.f}, {1.f, -1.f, 0.f});
-	quad->setProgram(billboardProgram);
-	planet->addModel(quad);
+	Program earthTextProgram("render.vert", "texture.frag");
+	Texture earthTextTexture("../Textures/Earth Text.png");
+	earthTextTexture.bind(11, earthTextProgram, "tex");
+	std::shared_ptr<QuadModel> earthTextQuad = std::make_shared<QuadModel>();
+	glm::vec3 lt = glm::vec3(earth->getPosition() + glm::vec3(-earth->getRadius(), earth->getRadius() * 2.2f, 0.f));
+	glm::vec3 rb = glm::vec3(earth->getPosition() + glm::vec3( earth->getRadius(), earth->getRadius() * 1.2f, 0.f));
+	earthTextQuad->createQuad(lt, rb);
+	earthTextQuad->setProgram(earthTextProgram);
+	earth->addModel(earthTextQuad);
 
-	spaceScene->addPlanet(planet);
-	
+	Program moonProgram("render.vert", "render.frag");
+	Texture moonTexture("../Textures/Moon.jpg");
+	moonTexture.bind(5, moonProgram, "tex");
+	std::shared_ptr<Planet> moon = std::make_shared<Planet>("Moon", glm::vec3(.15f, 0.f, 0.f), 0.025f, moonProgram);
+	moonProgram.setUniform("lightPosition", sun->getPosition());
+	moonProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(moon);
+
+	Program marsProgram("render.vert", "render.frag");
+	Texture marsTexture("../Textures/Mars.jpg");
+	marsTexture.bind(6, marsProgram, "tex");
+	std::shared_ptr<Planet> mars = std::make_shared<Planet>("Mars", glm::vec3(0.52f, 0.f, 0.f), .05f, marsProgram);
+	marsProgram.setUniform("lightPosition", sun->getPosition());
+	marsProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(mars);
+
+	Program jupiterProgram("render.vert", "render.frag");
+	Texture jupiterTexture("../Textures/Jupiter.jpg");
+	jupiterTexture.bind(7, jupiterProgram, "tex");
+	std::shared_ptr<Planet> jupiter = std::make_shared<Planet>("Jupiter", glm::vec3(4.2f, 0.f, 0.f), 1.12f, jupiterProgram);
+	jupiterProgram.setUniform("lightPosition", sun->getPosition());
+	jupiterProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(jupiter);
+
+	Program saturnProgram("render.vert", "render.frag");
+	Texture saturnTexture("../Textures/Saturn.jpg");
+	saturnTexture.bind(8, saturnProgram, "tex");
+	std::shared_ptr<Planet> saturn = std::make_shared<Planet>("Saturn", glm::vec3(8.52f, 0.f, 0.f), 0.94f, saturnProgram);
+	saturnProgram.setUniform("lightPosition", sun->getPosition());
+	saturnProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(saturn);
+
+	Program uranusProgram("render.vert", "render.frag");
+	Texture uranusTexture("../Textures/Uranus.jpg");
+	uranusTexture.bind(9, uranusProgram, "tex");
+	std::shared_ptr<Planet> uranus = std::make_shared<Planet>("Uranus", glm::vec3(18.21f, 0.f, 0.f), 0.4f, uranusProgram);
+	uranusProgram.setUniform("lightPosition", sun->getPosition());
+	uranusProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(uranus);
+
+	Program neptuneProgram("render.vert", "render.frag");
+	Texture neptuneTexture("../Textures/Neptune.jpg");
+	uranusTexture.bind(10, neptuneProgram, "tex");
+	std::shared_ptr<Planet> neptune = std::make_shared<Planet>("Neptune", glm::vec3(29.07f, 0.f, 0.f), 0.39f, neptuneProgram);
+	neptuneProgram.setUniform("lightPosition", sun->getPosition());
+	neptuneProgram.setUniform("lightPower", sun->getEmissivePower());
+	spaceScene->addPlanet(neptune);
+
 
 	SceneManager::getInstance()->addScene(spaceScene);
 	
