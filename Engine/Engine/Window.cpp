d20c9be4f0e33d0ced10552f79f849bd47df8174
Inyong <tlsdlsdyd98@gmail.com>
2024-06-08 15:46:48 +0900

    blending 기능 On

--- Engine/Engine/Window.cpp
+++ Engine/Engine/Window.cpp
@@ -92,6 +92,9 @@ void Window::display() const
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	while (!glfwWindowShouldClose(this->GLwindow))
 	{
 		glClearColor(0.f, 0.f, 0.f, 1.0f);
